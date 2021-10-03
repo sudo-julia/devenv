6a89c25b0de3c1485e7b81ac8cfad5f13cd416c6
sudo-julia <jlearning@tuta.io>
2021-10-02 22:37:58 -0700

    added empty dir checker

--- devenv/devenv.py
+++ devenv/devenv.py
@@ -8,13 +8,40 @@ import sys
 from devenv import SCRIPTS_DIR
 
 
+def has_files(path):
+    """Checks if a directory has files
+
+    Args:
+        path (Path): The path to the directory to check
+
+    Returns:
+        bool: True for success, False otherwise
+    """
+    # TODO: test this
+    files = [file for file in Path(path).iterdir()]
+    return bool(files)
+
+
 def print_error(msg):
-    """print a message to stderr"""
+    """Prints a message to stderr
+
+    Args:
+        msg (str): The message to print to stderr
+    """
     print(f"[ERR] {msg}", file=sys.stderr)
 
 
 def run_scripts(script_dir, lang, name):
-    """run scripts in a given dir"""
+    """Runs scripts in a given dir
+
+    Args:
+        script_dir (Path): The directory to search for scripts
+        lang (str): The main language of the project
+        name (str): The name of the project
+
+    Returns:
+        bool: True for success, False otherwise
+    """
     for script in script_dir.iterdir():
         if not os.access(script, os.X_OK):
             print_error(f"{script} is not executable!")
@@ -28,14 +55,17 @@ def run_scripts(script_dir, lang, name):
 
 
 def main():
-    """:"""
+    """Collect arguments and run the program"""
     parser = argparse.ArgumentParser()
     parser.add_argument("lang", help="the language of the project")
     parser.add_argument("name", help="the name of the project")
     args = parser.parse_args()
     lang_dir = Path(f"{SCRIPTS_DIR}/{args.lang}")
+    all_dir = Path(f"{SCRIPTS_DIR}/all")
 
-    if not lang_dir.exists() and not Path(f"{SCRIPTS_DIR}/all").exists():
+    if not (lang_dir.exists() or has_files(lang_dir)) and not (
+        Path(all_dir).exists() or has_files(all_dir)
+    ):
         # TODO: check for empty dirs
         print_error(
             f"Please populate '{lang_dir}' and/or '{SCRIPTS_DIR}/all' and run again."
